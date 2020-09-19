8f2c72bb64d45473d3656830355f7613def8a679
seasnail8169 <17166139+seasnail8169@users.noreply.github.com>
2020-09-19 11:19:07 +0100

    cleaned up ef shitty formatting

--- commands/fun/bruh.js
+++ commands/fun/bruh.js
@@ -5,7 +5,7 @@ const Discord = require("discord.js");
 var bruhpics = [
   "https://technofaq.org/wp-content/uploads/2016/02/bruh.jpg",
   "https://i.ytimg.com/vi/ZF57zsOWdB0/maxresdefault.jpg",
-  "http://www.quickmeme.com/img/07/07dd24bc150b74327b321009a2a6b3cb584f935249c0a86abc82cdbcb6b0a76e.jpg"
+  "http://www.quickmeme.com/img/07/07dd24bc150b74327b321009a2a6b3cb584f935249c0a86abc82cdbcb6b0a76e.jpg",
 ];
 
 module.exports = {
@@ -20,13 +20,13 @@ module.exports = {
         " ran the command: " +
         message.content
     );
-    
+
     const randBruh = bruhpics[Math.floor(Math.random() * chooseArr.length)];
 
     const embed = new MessageEmbed()
       .setColor(process.env.FAIL_COLOR)
       .setAuthor(`**Bruh Moment:**`)
-      .attachFiles(randBruh)
+      .attachFiles(randBruh);
     message.channel.send(embed);
   },
 };
