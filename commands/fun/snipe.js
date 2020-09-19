a8636491a3d4e6399fe6e5d5835e44f34e0782d7
seasnail8169 <17166139+seasnail8169@users.noreply.github.com>
2020-09-19 11:17:37 +0100

    fixed embed + formatting
    FUCKING HELL GET A PRETIFIER JESUS WHAT IS YPUR FORMATTING

--- commands/fun/snipe.js
+++ commands/fun/snipe.js
@@ -1,4 +1,4 @@
-//author ef1500
+//Author: ef1500
 const { MessageEmbed } = require("discord.js");
 const Discord = require("discord.js");
 
@@ -16,15 +16,12 @@ module.exports = {
     );
     message.delete();
 
-    //create list of possible outcomes
     var outcomes = ["You Missed.", "Headshot", "Target Eliminated.", "Misfire.", "よくできました！", "Great Shot!", "Target Lost.", "Get better eyes", "You killed an innocent suvillian"];
-
-
-        const randOut = outcomes[Math.floor(Math.random() * chooseArr.length)]; //choose an outcome
-
-        const embed = new MessageEmbed()
-          .setTitle(`***RESULTS***`)
-          .setDescription(randOut)
+    const randOut = outcomes[Math.floor(Math.random() * chooseArr.length)];
+    const embed = new MessageEmbed()
+        .setColor(process.env.GENERAL_COLOR)
+        .setAuthor(`***Result:***`)
+        .setDescription(randOut);
 
     message.channel.send(embed);
   },
