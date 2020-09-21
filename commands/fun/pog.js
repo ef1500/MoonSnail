47fc94a99d254287e9310aa22a65b53bbb628ae6
seasnail8169 <17166139+seasnail8169@users.noreply.github.com>
2020-09-21 02:50:29 +0100

    Update pog.js

--- commands/fun/pog.js
+++ commands/fun/pog.js
@@ -3,24 +3,21 @@ const { MessageEmbed } = require("discord.js");
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "pog",
-    category: "fun",
-    description: "Pogchamp",
-    usage: `pog`,
-    run: async (client, message, args) => {
-        console.log(
-            "ACTIVITY: " +
-            message.author.username +
-            " ran the command: " +
-            message.content
-        );
-        message.delete();
-        message.channel
-            .send(
-                "https://pics.me.me/thumb_pogchamp-43297127.png"
-            )
-            .then((msg) => {
-                msg.delete({ timeout: 60000 });
-            });
-    },
+  name: "pog",
+  category: "fun",
+  description: "Pogchamp",
+  run: async (client, message, args) => {
+    console.log(
+      "ACTIVITY: " +
+        message.author.username +
+        " ran the command: " +
+        message.content
+    );
+    message.delete();
+    message.channel
+      .send("https://pics.me.me/thumb_pogchamp-43297127.png")
+      .then((msg) => {
+        msg.delete({ timeout: 30000 });
+      });
+  },
 };
