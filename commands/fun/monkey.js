ed6880ad9d475ed59d0eeda377d250b38e6105dd
seasnail8169 <17166139+seasnail8169@users.noreply.github.com>
2020-09-21 02:50:48 +0100

    Update monkey.js

--- commands/fun/monkey.js
+++ commands/fun/monkey.js
@@ -3,31 +3,32 @@ const { MessageEmbed } = require("discord.js");
 const Discord = require("discord.js");
 
 var monkeypics = [
-    "https://i.ytimg.com/vi/M3XECvpSPNA/maxresdefault.jpg",
-    "https://i.imgflip.com/33kw.jpg",
-    "https://i.insider.com/59ca74582488496ef520430f?width=480",
+  "https://i.ytimg.com/vi/M3XECvpSPNA/maxresdefault.jpg",
+  "https://i.imgflip.com/33kw.jpg",
+  "https://i.insider.com/59ca74582488496ef520430f?width=480",
 ];
 
 module.exports = {
-    name: "monkey",
-    category: "fun",
-    description: "monkey brain",
-    run: async (client, message, args) => {
-        console.log(
-            "ACTIVITY: " +
-            message.author.username +
-            " ran the command: " +
-            message.content
-        );
-        message.delete();
-        const randMonkey = monkeypics[Math.floor(Math.random() * monkeypics.length)];
+  name: "monkey",
+  category: "fun",
+  description: "monkey brain",
+  run: async (client, message, args) => {
+    console.log(
+      "ACTIVITY: " +
+        message.author.username +
+        " ran the command: " +
+        message.content
+    );
+    message.delete();
+    const randMonkey =
+      monkeypics[Math.floor(Math.random() * monkeypics.length)];
 
-        const embed = new MessageEmbed()
-            .setColor(process.env.FAIL_COLOR)
-            .setAuthor(`MONKEY`)
-            .setImage(randMonkey);
-        message.channel.send(embed).then((msg) => {
-            msg.delete({ timeout: 30000 });
-        });
-    },
+    const embed = new MessageEmbed()
+      .setColor(process.env.FAIL_COLOR)
+      .setAuthor(`MONKEY`)
+      .setImage(randMonkey);
+    message.channel.send(embed).then((msg) => {
+      msg.delete({ timeout: 30000 });
+    });
+  },
 };
