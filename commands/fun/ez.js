e62b007238130400dca78b1732f76a7590f21d71
seasnail8169 <17166139+seasnail8169@users.noreply.github.com>
2020-09-21 02:51:07 +0100

    Update ez.js

--- commands/fun/ez.js
+++ commands/fun/ez.js
@@ -3,23 +3,19 @@ const { MessageEmbed } = require("discord.js");
 const Discord = require("discord.js");
 
 module.exports = {
-    name: "ez",
-    category: "fun",
-    description: "GG EZ",
-    run: async (client, message, args) => {
-        console.log(
-            "ACTIVITY: " +
-            message.author.username +
-            " ran the command: " +
-            message.content
-        );
-        message.delete();
-        message.channel
-            .send(
-                "https://i.imgur.com/vBgfzFL.png"
-            )
-            .then((msg) => {
-                msg.delete({ timeout: 10000 });
-            });
-    },
+  name: "ez",
+  category: "fun",
+  description: "GG EZ",
+  run: async (client, message, args) => {
+    console.log(
+      "ACTIVITY: " +
+        message.author.username +
+        " ran the command: " +
+        message.content
+    );
+    message.delete();
+    message.channel.send("https://i.imgur.com/vBgfzFL.png").then((msg) => {
+      msg.delete({ timeout: 10000 });
+    });
+  },
 };
